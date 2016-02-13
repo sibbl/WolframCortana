500a5160de1a09009e9a81a0e72d0c142c781278
Sebastian Müller <me@sibbl.net>
2016-02-13 17:07:09 +0100

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Windows.ApplicationModel.Activation;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace WolframCortana
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
+                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
+                Microsoft.ApplicationInsights.WindowsCollectors.Session);
+            this.InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            var rootFrame = Window.Current.Content as Frame;
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+            }
+            Window.Current.Activate();
+        }
+
+        protected override async void OnActivated(IActivatedEventArgs args)
+        {
+            base.OnActivated(args);
+
+            if (args.Kind == ActivationKind.VoiceCommand)
+            {
+                var voiceArgs = args as VoiceCommandActivatedEventArgs;
+
+                var props = voiceArgs.Result.SemanticInterpretation.Properties;
+                var query = props["query"][0];
+                var escapedQuery = Uri.EscapeDataString(query);
+
+                await Launcher.LaunchUriAsync(
+                    new Uri(string.Format("wolframalpha://query?input={0}", escapedQuery)),
+                    new LauncherOptions
+                    {
+                        FallbackUri = new Uri(string.Format("http://www.wolframalpha.com/input/?i={0}", escapedQuery)),
+                    }
+                );
+            }
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Windows.ApplicationModel.VoiceCommands;
+using Windows.Storage;
+using Windows.UI.Xaml.Controls;
+
+namespace WolframCortana
+{
+    public sealed partial class MainPage : Page
+    {
+        private const string VoiceCommandPath = "VoiceCommands.xml";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            InstallVoiceCommandDefinitions();
+        }
+
+        private async void InstallVoiceCommandDefinitions()
+        {
+            var firstTime = VoiceCommandDefinitionManager.InstalledCommandDefinitions.Count == 0;
+            MessageText.Text = firstTime ? "Integrating into Cortana..." : "Updating Cortana integration...";
+            try
+            {
+                var storageFile =
+                    await StorageFile.GetFileFromApplicationUriAsync(
+                        new Uri("ms-appx:///" + VoiceCommandPath));
+                await VoiceCommandDefinitionManager
+                        .InstallCommandDefinitionsFromStorageFileAsync(storageFile);
+                MessageText.Text = firstTime ? "Cortana integration is ready." : "Cortana integration is up to date.";
+            }
+            catch (Exception e)
+            {
+                MessageText.Text = "Cortana, we have a problem.";
+            }
+        }
+    }
+}
